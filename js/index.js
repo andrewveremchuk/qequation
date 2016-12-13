0db280820f4d58b6f1fed40dc07021afb2a3eb0d
Andrew Veremchuk <andrewveremchuk@meta.ua>
2016-12-13 14:24:40 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -15,9 +15,9 @@ function quad_equation(discr) {
 }    
 
     
-    var a = Number(prompt('Vvedit a'));
-    var b = Number(prompt('Vvedit b'));
-    var c = Number(prompt('Vvedit c'));
+    var a = Number(prompt('For solving square equation input a'));
+    var b = Number(prompt('For solving square equation input b'));
+    var c = Number(prompt('For solving square equation input c'));
     var discr=b*b-4*a*c;
     
     var result = quad_equation(discr);
